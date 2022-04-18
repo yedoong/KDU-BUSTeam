c7c3766c63a2530c22e7ad88a583f607c41c9e69
예원 <88189531+yedoong@users.noreply.github.com>
2022-04-19 01:37:44 +0900

    Update pop.js

--- jS/pop.js
+++ jS/pop.js
@@ -3,12 +3,20 @@ $(function(){
         modalClose();
         //컨펌 이벤트 처리
     });
-    $("#modal-open").click(function(){        $("#popup").css('display','flex').hide().fadeIn();
+    $("#modal-open-1").click(function(){        $("#popup").css('display','flex').hide().fadeIn();
     });
-    $("#close").click(function(){
+    $("#modal-open-2").click(function(){        $("#popup").css('display','flex').hide().fadeIn();
+    });
+    $("#modal-open-3").click(function(){        $("#popup").css('display','flex').hide().fadeIn();
+    });
+    $("#modal-open-4").click(function(){        $("#popup").css('display','flex').hide().fadeIn();
+    });
+    $("#close").click(function(){ // 닫는거
         modalClose();
     });
-    function modalClose(){
+    function modalClose(){ // 천천히 닫히는 거
       $("#popup").fadeOut();
     }
   });
+
+  
