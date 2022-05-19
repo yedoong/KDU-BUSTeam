7ca6168593a355603318e7aa58405e3f207ffaa3
잇장 <95205177+ITJANG@users.noreply.github.com>
2022-05-20 00:55:26 +0900

    Update UserDAO.java

--- KDU_BUS/KDU_BUS_Test/src/user/UserDAO.java
+++ KDU_BUS/KDU_BUS_Test/src/user/UserDAO.java
@@ -6,27 +6,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class UserDAO {
-	private Connection conn; // 데이터베이스 접근하기 위한 객체
+	private Connection conn;
 	private PreparedStatement pstmt;
-	private ResultSet rs; // 정보 담을 수 있는 객체
+	private ResultSet rs;
 	
-	// 데이터베이스 커넥션을 위함 - userdatabase의 user_data table
 	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://3.39.182.65:3306/user_database";
-			String dbID = "yijeong";
-			String dbPassword = "1234";
+			String dbURL = "jdbc:mysql://43.200.37.178:3306/bus_database";
+            String dbID = "yijeong";
+            String dbPassword = "1234";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
-			e.printStackTrace(); //오류발생시 오류출력
+			e.printStackTrace();
 		}
 	}
 	
-	// 실제 로그인 시도하는 함수
 	public int login(String user_id, String user_name) { //로그인 정보(user_id)
-		
-		//실제 SQL에서 사용할 명령어 작성
 		String SQL = "SELECT user_name FROM user_data WHERE user_id = ?";
 		try {
 			pstmt = conn.prepareStatement(SQL);
